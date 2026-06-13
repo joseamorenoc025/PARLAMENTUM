c85368b5f84fa555fab21fc2d87b2effc442c07f
jose-dev92 <josealb_dev@proton.me>
2026-06-13 07:47:34 -0400

    Portal: visual polish - dark mode, icons, a11y, CSS bugs, cache v5

--- views/helpers.js
+++ views/helpers.js
@@ -5,10 +5,10 @@ export function escapeHTML(str) {
 
 export function popularityBadge(totalDl) {
   if (totalDl >= 10) {
-    return `<span class="badge badge-popular"><i data-lucide="flame" style="width:10px;height:10px;display:inline;margin-right:2px;"></i> Popular</span>`;
+    return `<span class="badge badge-popular"><i data-lucide="flame" class="icon-xxs badge-icon"></i> Popular</span>`;
   }
   if (totalDl >= 3) {
-    return `<span class="badge badge-interest"><i data-lucide="trending-up" style="width:10px;height:10px;display:inline;margin-right:2px;"></i> Interés Reciente</span>`;
+    return `<span class="badge badge-interest"><i data-lucide="trending-up" class="icon-xxs badge-icon"></i> Interés Reciente</span>`;
   }
   return '';
 }
